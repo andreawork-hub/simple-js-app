e38c1c787df9683c4bcd2057f9ae503de56df7af
andreawork-hub <andrea.vitova@hotmail.com>
2022-10-23 10:45:10 +0200

    Updated Scripts.js file
    > cancelled the existing content
    > created a new variable pokemonList
    > created 3 objects in pokemonList e.g., Pokemons with 3 keys and assigned the value for each key

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
-alert('Hello World')
-
-let favouriteFood='Spaghetti' ; 
-document.write(favouriteFood); 
+let pokemonList=[
+    {
+        name:"Butterfree",
+        height:"1.1",
+        type:['bug','flying']
+    },
+    {
+        name:"Meowth",
+        height:"0.4",
+        type:'normal',
+    },
+    {
+        name:"Gothitelle",
+        height:"1.5",
+        type:'psychic',
+    },
+] ; 
+document.write(pokemonList) ; 
