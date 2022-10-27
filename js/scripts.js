e9822694831d79b4daab62826219939133e377a6
andreawork-hub <andrea.vitova@hotmail.com>
2022-10-28 00:30:49 +0200

    Updated files scripts / index
    > created a (for) loop with conditional
    > used document.write (loop is visible on website´s DOM)
    > inspected code > no errors

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,13 @@ let pokemonList=[
         type:'psychic',
     },
 ] ; 
-document.write(pokemonList) ; 
+
+for (let index=0 ; index < pokemonList.length ; index++ ) {
+    if (pokemonList[index].height > 0.5 && pokemonList[index].height < 1.2) {
+        document.write(pokemonList[index].name + " (height: " + pokemonList[index].height + ")" + "<br>") ;
+    }else if (pokemonList[index].height < 0.5) {
+        document.write(pokemonList[index].name + " (height: " + pokemonList[index].height + ")" + "<br>") ;
+    }else {
+        document.write(pokemonList[index].name + " (height: " + pokemonList[index].height + ") - Wow, that's big!") 
+    }
+}
