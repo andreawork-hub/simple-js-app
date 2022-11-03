85c86b41de64d35678799f42c1723611e5e4ae8d
andreawork-hub <andrea.vitova@hotmail.com>
2022-11-03 14:46:45 +0100

    Updated JavaScript file
    // used a forEach() function instead of the for loop
    // function iterates over each item in pokemonList
    // function prints the details of each item
    // arrow function

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,10 @@ let pokemonList=[
     },
 ] ; 
 
-for (let index=0 ; index < pokemonList.length ; index++ ) {
+// created a for loop that iterates over each item in pokemonList
+// added a conditional to check if the height is above a certain value, with note “Wow, that’s big!”
+
+/*for (let index=0 ; index < pokemonList.length ; index++ ) {
     if (pokemonList[index].height > 0.5 && pokemonList[index].height < 1.2) {
         document.write(pokemonList[index].name + " (height: " + pokemonList[index].height + ")" + "<br>") ;
     }else if (pokemonList[index].height < 0.5) {
@@ -24,4 +27,11 @@ for (let index=0 ; index < pokemonList.length ; index++ ) {
     }else {
         document.write(pokemonList[index].name + " (height: " + pokemonList[index].height + ") - Wow, that's big!") 
     }
-}
+} 
+*/ 
+
+// used a forEach() function instead of the for loop 
+// function iterates over each item in pokemonList, prints the details of each item 
+// arrow function 
+
+pokemonList.forEach (pokemonList => console.log (pokemonList)) ; 
